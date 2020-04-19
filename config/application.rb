f25171283958a4c8096f287c1699e432d7547e79
Edgar Gonzalez <edgargdev@gmail.com>
2020-04-19 11:54:05 -0500

    add TCP logging to Splunk

--- config/application.rb
+++ config/application.rb
@@ -43,18 +43,18 @@ module ProjectOrganizerBackend
 
     OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-    config.rails_semantic_logger.started    = true
-    config.rails_semantic_logger.processing = true
-    config.rails_semantic_logger.rendered   = true
-    config.semantic_logger.backtrace_level = :info
-    config.log_level = :debug
+    # config.rails_semantic_logger.started    = true
+    # config.rails_semantic_logger.processing = true
+    # config.rails_semantic_logger.rendered   = true
+    # config.semantic_logger.backtrace_level = :info
+    config.log_level = :trace
     config.rails_semantic_logger.format = :json
-
-    config.log_tags = {
-      request_id: :request_id,
-      ip:         :remote_ip,
-      user:       -> request { request.cookie_jar['login'] },
-      correlation_id: "xxxx-xxxx-xxxx-xxxx"
-    }
+    
+    # config.log_tags = {
+    #   request_id: :request_id,
+    #   ip:         :remote_ip,
+    #   user:       -> request { request.cookie_jar['login'] },
+    #   correlation_id: "xxxx-xxxx-xxxx-xxxx"
+    # }
   end
 end
--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,7 +1,18 @@
-SemanticLogger.add_appender(
-    appender: :splunk_http,
-    url:      'https://splunk:8088/services/collector/event',
-    token:    '28819799-f892-401e-bffa-34f7b43c7d6a'
-)
+module Logentries
+    class Formatter < SemanticLogger::Formatters::Json
+      attr_accessor :token
+  
+      def initialize(token)
+        @token = token
+      end
+  
+      def call(log, logger)
+        "#{token} #{super(log, logger)}"
+      end
+    end
+end
+
+formatter = Logentries::Formatter.new('1314cca4-91d1-4588-9faa-1f31fd332d30')
+SemanticLogger.add_appender(appender: :tcp, server: 'splunk:4000', ssl: false, formatter: formatter)
 
 # SemanticLogger.add_appender(file_name: 'sample.log')
